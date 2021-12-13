d38a76a2b1d13c914c59bf013154401fe07d1be6
joumene010 <93882705+joumene010@users.noreply.github.com>
2021-12-13 19:01:47 +0100

    Update Main.java

--- src/tp3/ex1/Main.java
+++ src/tp3/ex1/Main.java
@@ -1,19 +1,33 @@
 package tp3.ex1;
 
+import java.util.Scanner;
+
 public class Main {
 
 
-    // create a class that extends thread and another one that implements runnable
-    // they should print 'my thread is running' when running
-    // when both terminated, print 'it s over !!'
-
-    public static void main(String[] args) {
-
-
-
-        //output:
-        //my thread is running
-        //my thread is running
-        //it s over !!
+    static class Methode1 extends Thread {
+        @Override
+        public void run() {
+            System.out.println("my thread is running");
+        }
     }
-}
+    static class Methode2 implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("my thread is running");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+            Methode1 t1 = new Methode1();
+            t1.start();
+            Methode2 t = new Methode2();
+            Thread t2 = new Thread(t);
+            t2.start();
+            t1.join();
+            t2.join();
+            System.out.println("it s over !!");
+        }
+
+    }
+
